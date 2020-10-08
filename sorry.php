7a6019d1efa8dc60b8afb03656b43c57e932de3c
panwaffle36 <70587222+panwaffle36@users.noreply.github.com>
2020-10-07 21:52:32 -0400

    Create sorry.php

---
+++ sorry.php
@@ -0,0 +1,4 @@
+I've been pushing this class and its assignments to the back of my mind ever since I got completely frustrated and stuck trying to finish the assignment that was due on September 17th.
+I shut down when I get too frustrated, and intimidated by the thought of trying to learn all this technology at once, and it only gets worse the longer I wait.
+And then I vent here instead of asking you directly for help because I'd feel bad about burdening you with problems I brought on myself by being too attention defecit to pay any in class.
+I just don't know how to work things I missed into my schedule, since, now that the due date's passed, there's no solid deadline anymore.
